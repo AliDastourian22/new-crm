785ede1bff3b224ddd039bc048d1916809d03c6a
Ali Dastourin <alidastourian@gmail.com>
2024-04-18 15:09:23 +0300

    all done

--- class.py
+++ class.py
@@ -15,11 +15,27 @@ class Detail():
         self.Engine = Engine
         self.Option = Option
         self.type = type
-        
-class Nissan(Detail):
-    def __init__(self, model="", productCreate_Y=0, Price=0, Company="" , Engine="", Option="", type=""):
-        super().__init__(model, productCreate_Y, Price, Company, Engine,Option,type)
 
+    def setproductCreate_Y(self,value):
+        if value>1960:
+                    self.productCreate_Y=value
+
+    def getproductCreate_Y(self):
+
+        return self.productCreate_Y
+    def setprice(self,value):
+        if value>200000:
+            self.Price = value
+    def getprice(self):
+        return self.Price
+
+    productCreate_Y=property(setproductCreate_Y,getproductCreate_Y)
+    price=property(getprice,setprice)
+class Nissan(Detail):
+    creator=""
+    def __init__(self,creator="", model="", productCreate_Y=0, Price=0, Company="" , Engine="", Option="", type=""):
+        super().__init__(model, productCreate_Y, Price, Company, Engine,Option,type)
+        self.creator = creator
 
 class Lamborgini(Detail):
     def __init__(self, model="", productCreate_Y=0, Price=0, Company="" , Engine="", Option="", type=""):
@@ -27,8 +43,7 @@ class Lamborgini(Detail):
 
 
 
-
-nis=Nissan(model="maxima",productCreate_Y=1396,Price=20000,Company="NISSAN",Engine="V-8",Option="P",type="sport")
+nis=Nissan(model="maxima",productCreate_Y=1396,Price=2000000,Company="NISSAN",Engine="V-8",Option="P",type="sport")
 
 
 
